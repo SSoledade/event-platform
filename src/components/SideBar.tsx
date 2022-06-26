20e162b81faf5ceb2c99468ac0ed9125869e196d
SSoledade <soledade.contato@gmail.com>
2022-06-25 23:37:59 -0300

    event page completion

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -28,11 +28,11 @@ export function SideBar() {
 
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
-      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+      <span className="font-bold text-2xl pb-6 mb-5 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
 
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-6">
         {data?.lessons.map((lesson) => {
           return (
             <Lesson
